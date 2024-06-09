b41788f6053859914697ddfaccbae2ed92f69f49
daliborstakic <stakicdalibor64@gmail.com>
2024-06-09 17:49:01 +0200

    Add login form, new font family and placeholder testing code

--- blogpost-front/src/app/components/login/login.component.ts
+++ blogpost-front/src/app/components/login/login.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  public loginForm: FormGroup = new FormGroup({
+    username: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    password: new FormControl<string>('', [
+      Validators.required,
+      Validators.pattern(
+        '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$'
+      ),
+    ]),
+  });
 
+  public ispis: any = null;
+
+  submit() {
+    console.log(this.loginForm.value);
+    this.ispis = this.loginForm.value;
+  }
 }
