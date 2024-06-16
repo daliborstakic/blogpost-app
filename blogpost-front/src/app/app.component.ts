a6225b6605cbded27097ccb39045c4a4b6c49b5a
daliborstakic <stakicdalibor64@gmail.com>
2024-06-16 13:28:34 +0200

    Add navbar containing links and routes for those links
    The navbar is hidden if the selected component is the LoginComponent

--- blogpost-front/src/app/app.component.ts
+++ blogpost-front/src/app/app.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, LoginComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    LoginComponent,
+    HttpClientModule,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'blogpost-front';
+  showNavbar = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showNavbar = event.url !== '/login';
+      }
+    });
+  }
 }
