5cbcdc2d049cae59132eec047b1efcf5a9b127bc
daliborstakic <stakicdalibor64@gmail.com>
2024-06-23 20:40:05 +0200

    Add modal popper for showing user profile

--- blogpost-front/src/app/components/blogpost/blogpost.component.ts
+++ blogpost-front/src/app/components/blogpost/blogpost.component.ts
@@ -1,25 +1,49 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
 import { Blogpost } from '../../model/blogpost';
 import { UserService } from '../../services/user.service';
 import { User } from '../../model/user';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { CommentListComponent } from '../comment-list/comment-list.component';
+import { AddCommentComponent } from '../add-comment/add-comment.component';
+import { AuthenticationService } from '../../services/authentication.service';
+import { CommentService } from '../../services/comment.service';
+import { Comment } from '../../model/comment';
+import { CommonModule } from '@angular/common';
+import { LikeService } from '../../services/like.service';
+import { MiniProfileComponent } from '../mini-profile/mini-profile.component';
 
 @Component({
   selector: 'app-blogpost',
   standalone: true,
-  imports: [CommentListComponent],
+  imports: [
+    CommentListComponent,
+    AddCommentComponent,
+    CommonModule,
+    MiniProfileComponent,
+  ],
   templateUrl: './blogpost.component.html',
   styleUrl: './blogpost.component.css',
 })
 export class BlogpostComponent {
   @Input() public blogpost!: Blogpost;
-  public liked = false;
+  public liked!: boolean;
   public author: User | undefined;
+  public currentUser!: User | null;
+  public comments$: BehaviorSubject<Comment[]> = new BehaviorSubject<Comment[]>(
+    []
+  );
+  public showProfile = false;
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private authService: AuthenticationService,
+    private commentService: CommentService,
+    private likeService: LikeService
+  ) {}
 
   ngOnInit(): void {
+    this.currentUser = this.authService.currentUserValue;
+
     if (this.blogpost && this.blogpost.userId) {
       this.userService
         .getUserInfoById(this.blogpost.userId)
@@ -27,9 +51,44 @@ export class BlogpostComponent {
           this.author = user;
         });
     }
+
+    this.fetchComments();
+
+    if (this.currentUser?.id !== undefined)
+      this.likeService
+        .isLike(this.blogpost.id, this.currentUser?.id)
+        .subscribe((liked) => {
+          this.liked = liked;
+        });
+  }
+
+  fetchComments() {
+    this.commentService
+      .getCommentsByPost(this.blogpost.id)
+      .subscribe((comments) => {
+        this.comments$.next(comments);
+      });
   }
 
   toggleLike() {
     this.liked = !this.liked;
+
+    if (this.currentUser?.id === undefined) return;
+
+    if (this.liked) {
+      this.blogpost.likes++;
+      this.likeService
+        .likePost(this.blogpost.id, this.currentUser?.id)
+        .subscribe(() => {
+          console.log('The post was liked successfully!');
+        });
+    } else {
+      this.blogpost.likes--;
+      this.likeService
+        .unlikePost(this.blogpost.id, this.currentUser?.id)
+        .subscribe(() => {
+          console.log('The post was unliked successfully!');
+        });
+    }
   }
 }
