ee5d3e8567d78dde8c2944b82ea337c0988db05b
daliborstakic <stakicdalibor64@gmail.com>
2024-06-23 20:38:32 +0200

    Separate navbar into independent component
    Add floating button for scrolling to top

--- blogpost-front/src/app/app.component.ts
+++ blogpost-front/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnInit } from '@angular/core';
 import {
   NavigationEnd,
   Router,
@@ -9,6 +9,18 @@ import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { AuthenticationService } from './services/authentication.service';
+import { User } from './model/user';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
@@ -18,20 +30,38 @@ import { CommonModule } from '@angular/common';
     HeaderComponent,
     LoginComponent,
     HttpClientModule,
-    CommonModule,
     RouterModule,
+    BsDropdownModule,
+    CommonModule,
+    ReactiveFormsModule,
+    NavbarComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   showNavbar = true;
+  showScrollButton = false;
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        this.showNavbar = event.url !== '/login';
+        const excludedRoutes = ['/login', '/register', '/'];
+        this.showNavbar = !excludedRoutes.includes(event.url);
       }
     });
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const scrollY = window.scrollY;
+    this.showScrollButton = scrollY > 100;
   }
 }
