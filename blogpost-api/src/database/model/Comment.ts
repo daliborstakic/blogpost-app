e06a9d343e159e81b944c9de85335e38c74a17a0
daliborstakic <stakicdalibor64@gmail.com>
2024-06-23 20:37:15 +0200

    Fix error with parsing and passing values

--- blogpost-api/src/database/model/Comment.ts
+++ blogpost-api/src/database/model/Comment.ts
@@ -15,21 +15,21 @@ export class CommentRepository {
     this.db = db;
   }
 
-  public addComment(
-    blogpostId: number,
-    userId: number,
-    content: string
-  ): Promise<void> {
+  public addComment(comment: Comment): Promise<number> {
     return new Promise((resolve, reject) => {
-      const query = `INSERT INTO comments (blogpostId, userId, content) VALUES (?, ?, ?)`;
+      const insertQuery = `
+        INSERT INTO comments (blogpostId, userId, content)
+        VALUES (?, ?, ?);
+      `;
+
       this.db.run(
-        query,
-        [blogpostId, userId, content],
+        insertQuery,
+        [comment.blogpostId, comment.userId, comment.content],
         function (err: Error | null) {
           if (err) {
             reject(err);
           } else {
-            resolve();
+            resolve(this.lastID);
           }
         }
       );
@@ -49,14 +49,14 @@ export class CommentRepository {
     });
   }
 
-  public deleteComment(id: number): Promise<void> {
+  public deleteComment(id: number): Promise<number> {
     return new Promise((resolve, reject) => {
       const query = `DELETE FROM comments WHERE id = ?`;
       this.db.run(query, [id], function (err: Error | null) {
         if (err) {
           reject(err);
         } else {
-          resolve();
+          resolve(this.changes);
         }
       });
     });
--- blogpost-front/src/app/services/comment.service.ts
+++ blogpost-front/src/app/services/comment.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { Comment } from '../model/comment';
 
@@ -18,50 +18,47 @@ export class CommentService {
   }
 
   getCommentsByPost(blogpostId: number): Observable<Comment[]> {
-    return this.http
-      .get<Comment[]>(`/api/comments/${blogpostId}`)
-      .pipe(catchError(this.handleError));
+    return this.http.get<Comment[]>(`/api/comments/${blogpostId}`).pipe(
+      tap((comments) => {
+        this.commentsSubject.next(comments);
+      }),
+      catchError(this.handleError)
+    );
   }
 
   addComment(
     blogpostId: number,
     userId: number,
     content: string
-  ): Observable<void> {
+  ): Observable<any> {
     return this.http
-      .post<void>(`/api/comment`, { blogpostId, userId, content })
+      .post<any>('/api/comment', {
+        blogpostId: blogpostId,
+        userId: userId,
+        content: content,
+      })
       .pipe(
-        tap(() => this.reloadComments(blogpostId)),
+        tap((newComment: Comment) => {
+          const updatedComments = [...this.commentsSubject.value, newComment];
+          this.commentsSubject.next(updatedComments);
+        }),
         catchError(this.handleError)
       );
   }
 
-  deleteComment(id: number, blogpostId: number): Observable<void> {
-    return this.http.delete<void>(`/api/comment/${id}`).pipe(
-      tap(() => this.reloadComments(blogpostId)),
+  deleteComment(id: number): Observable<any> {
+    return this.http.delete<any>(`/api/comment/${id}`).pipe(
+      tap(() => {
+        const updatedComments = this.commentsSubject.value.filter(
+          (comment) => comment.id !== id
+        );
+        this.commentsSubject.next(updatedComments);
+      }),
       catchError(this.handleError)
     );
   }
 
-  updateComments(comments: Comment[]) {
-    this.commentsSubject.next(comments);
-  }
-
   getCommentsObservable(): Observable<Comment[]> {
     return this.commentsSubject.asObservable();
-  }
-
-  reloadComments(blogpostId: number) {
-    this.getCommentsByPost(blogpostId).subscribe({
-      next: (comments: Comment[]) => {
-        this.updateComments(comments);
-      },
-      error: (err) => {
-        console.error('Failed to load comments', err);
-      },
-      complete: () => {
-        console.log('Finished loading comments!');
-      },
-    });
   }
 }
